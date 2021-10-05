e07c5d825a6a152d7291cb377d7e91ab4870ebd3
raydawnzen <91921595+raydawnzen@users.noreply.github.com>
2021-10-05 11:15:34 +0100

    Update and rename 2021-10-05.js to 2021-10-06.js

---
+++ src/js/data/2021-10-06.js
@@ -0,0 +1,577 @@
+dataSetVersion = "2021-10-06"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "",
+    img: "2cgFcFd.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "OZEhBcH.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Vm16nbX.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "qNDJiRF.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "SwBtL3Y.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "euYZfNJ.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "hLBx65X.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "RnExYVz.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "A9cTR0D.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "ZgTbCFQ.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "sycYgch.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "3DjGffY.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "JORRb98.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "LAzs8iR.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "86lDe6F.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "ftuPH1k.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "qZNSqMQ.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "VTjUPqR.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "28ao1vq.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "6Ndgkpc.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "DFqC6wy.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "fJnnoDw.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "3CEAXip.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Njm7DjI.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "d8cdy5s.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "px83W4d.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "a5tgsVG.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "XN5xiEc.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "hAvoDDD.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "mimn8VK.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "hmW0X1Q.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "aRJntFs.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "WHOYeys.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "v6kAvPC.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Uqzk24D.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "o2gGVKb.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "ZeHUDZr.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "ZsBX4GT.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "GVoyyjV.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "iuCaNRz.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "uy0Ncof.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "QLkOsgD.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "5I6KkSM.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "DDPd0WO.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "w2Bf9d7.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "queztHn.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "axsg9pH.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "5hQuK80.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "r54RiLD.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "e43dbuu.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "suC1DuH.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "ijJlf7j.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "pZhIigs.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "SsOik4w.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "DsZgHAz.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "T3fHW9X.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "MxTcI2e.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "piS0gJj.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "gHDUD27.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "9CPuwy9.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "qfqL1M5.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "2DENOyS.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Gl5NGFw.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "UjWJr1F.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Ev2pGUX.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "6bzVaEz.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "mTOqGww.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "9cfAHEx.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "90gI1E2.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "EmFzN27.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "hCww6lJ.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "m5zQysZ.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "eZS597p.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "WnT7sEk.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "8XKZ7uT.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "sOHeNKx.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "weinErg.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "mYSQh8Y.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "7yuDhs7.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "FWbf0D8.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "f2lYoOW.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "kl1BUdk.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "LtGsKk9.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "I7TBKU8.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "qtnE0Hz.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Mfee8kD.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "f7RYaF5.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "GK0b0M2.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "DGjvBmU.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "BDOxJtp.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "icySAOf.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "6MMHa7E.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "yjXBtxx.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "nKRmqS7.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "b4vOzfp.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "RiUpUbv.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "9xXZDZD.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "pWecDMI.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "edZaWMr.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "YInUyxC.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "6PaaxG6.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "FuuB5DR.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "OJN4qtj.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "SUct96r.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "rp4WCAS.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Ji5nORt.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Me53S0l.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "sXSvfwc.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "xsxW8i0.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "QQgHYn0.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "crjXef2.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "sbYIdqP.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "AoEKaN0.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "1hgzoKk.png",
+    opts: {}
+  }
+];
