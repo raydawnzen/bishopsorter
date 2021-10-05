348fdd5ae72a1b8b1610885cba84241da322dfdd
raydawnzen <91921595+raydawnzen@users.noreply.github.com>
2021-10-05 01:52:29 +0100

    Create 2021-10-05.js

---
+++ src/js/data/2021-10-05.js
@@ -0,0 +1,92 @@
+dataSetVersion = "2021-10-05"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "",
+    img: "2cgFcFd.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "OZEhBcH.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "Vm16nbX.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "qNDJiRF.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "SwBtL3Y.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "euYZfNJ.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "hLBx65X.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "RnExYVz.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "A9cTR0D.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "ZgTbCFQ.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "sycYgch.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "3DjGffY.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "JORRb98.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "LAzs8iR.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "86lDe6F.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "ftuPH1k.png",
+    opts: {}
+  },
+  {
+    name: "",
+    img: "qZNSqMQ.png",
+    opts: {}
+  }
+];
